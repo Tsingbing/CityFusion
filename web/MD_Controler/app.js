acdbcad1259b3e32d775805f5e0e03218bd52432
qbingx <767575850@qq.com>
2017-06-27 17:43:52 +0800

    更新

--- web/MD_Controler/app.js
+++ web/MD_Controler/app.js
@@ -10,15 +10,17 @@ var app = express();
 var path = require('path');
 
 
-app.use(function(req, res,next){
-	//console.log("%s %s", req.method, req.url);
-	next();
+app.use(function(req, res, next) {
+  //console.log("%s %s", req.method, req.url);
+  next();
 });
 
-app.use(express.static( __dirname+'/'));
+app.use(express.static(__dirname + '/'));
 
 //body 解析器
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/about", function(request, response) {
   response.end("Welcome to the about page!");
@@ -31,21 +33,20 @@ amqp.connect('amqp://localhost', function(err, conn) {
     // var msg = args.slice(1).join(' ') || 'Hello World!';
     // var severity = (args.length > 0) ? args[0] : 'info';
     var severity = 'messages_key';
-		ch.assertExchange(ex, 'direct', {durable: false});
+    ch.assertExchange(ex, 'direct', {
+      durable: false
+    });
 
-		//接收web页面的信息
-		app.post('/api/orders', function(req,res) {
-			var data = req.body;
-			//var data = JSON.stringify(req.body);
-			console.log(data.name +"   "+ data.drink);
-			//
-			// ch.publish(ex, severity, new Buffer(msg));
-			ch.publish(ex, severity, new Buffer(data.name));
-			res.send("yes");
-		});
-		
+    //接收web页面的信息
+    app.post('/api/orders', function(req, res) {
+      var data = req.body;
+      //var data = JSON.stringify(req.body);
+      console.log(data.name + "   " + data.drink);
+      // ch.publish(ex, severity, new Buffer(msg));
+      ch.publish(ex, severity, new Buffer(data.name));
+      res.send("yes");
+    });
   });
-
   //setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
 
